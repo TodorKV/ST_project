b34301607c14ed32159f15e3aa23a768f817499a
neli17 <93739274+neli17@users.noreply.github.com>
2022-12-14 20:01:11 +0200

    All controllers

---
+++ st_be/src/main/java/com/stproject/st_be/controllers/impl/CommentController.java
@@ -0,0 +1,16 @@
+package ccom.stproject.st_be.controllers.impl;
+
+import com.ft.controllers.base.BaseAbstrController;
+import com.ft.dto.CommentDto;
+import com.ft.service.base.BaseService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/comment")
+public class CommentController extends BaseAbstrController<CommentDto> {
+
+    public CommentController(BaseService<CommentDto> service) {
+        super(service);
+    }
+}
